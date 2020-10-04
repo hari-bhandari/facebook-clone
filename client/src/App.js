c37d24cc8e3bd8f4f9980fc1d3e8e3ca0e1653a7
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-04 17:55:33 +0100

    added plan

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+    {/*  Header*/}
+    {/*App Body*/}
+      {/*  SideBar*/}
+      {/*Feed*/}
+      {/*Widgets*/}
     </div>
   );
 }
