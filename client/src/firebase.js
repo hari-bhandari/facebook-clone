30e3b7b78e36932f22a1542747a159bdda058dbf
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-04 17:54:12 +0100

    added firebase.js

---
+++ client/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDKrpNRqF3Bf5Y2NxyX-wDmfU6jKKuUf0",
+    authDomain: "facebook702.firebaseapp.com",
+    databaseURL: "https://facebook702.firebaseio.com",
+    projectId: "facebook702",
+    storageBucket: "facebook702.appspot.com",
+    messagingSenderId: "747913944398",
+    appId: "1:747913944398:web:793fcc7d64eda07eb3521f",
+    measurementId: "G-T3YSJXJFBG"
+};
