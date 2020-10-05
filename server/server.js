21659b9580826765d02ea78ee08d693e24391c9f
Hari Bhandari <61126375+hari-bhandari@users.noreply.github.com>
2020-10-05 10:08:07 +0100

    connected to db

--- server/server.js
+++ server/server.js
@@ -3,8 +3,12 @@ import helmet from 'helmet'
 import cors from 'cors'
 import dotenv from 'dotenv'
 const app=express()
+import connectDB from './config/db'
 const PORT= process.env.PORT||5000
 ////////////////////////////////
+//connecting to db
+dotenv()
+//config
 app.set('trust proxy',true);
 dotenv.config({path:'./config/config.env'})
 //implementing helmet
