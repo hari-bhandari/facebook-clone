a4adc570440a031d2ef63b60e52269ddc25e7c21
Hari Bhandari <61126375+hari-bhandari@users.noreply.github.com>
2020-10-05 09:54:15 +0100

    added config

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
+import dotenv from 'dotenv'
 const app=express()
 const PORT= process.env.PORT||5000
 ////////////////////////////////
-
+app.set('trust proxy',true);
+dotenv.config({path:'./config/config.env'})
 //implementing helmet
 app.use(helmet())
 //cors
