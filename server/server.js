dc74ecc8e471af84005a7078a4cfaf196e451e7b
Hari Bhandari <61126375+hari-bhandari@users.noreply.github.com>
2020-10-05 09:48:33 +0100

    implemented cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import helmet from 'helmet'
+import cors from 'cors'
 const app=express()
 const PORT= process.env.PORT||5000
 ////////////////////////////////
 
 //implementing helmet
 app.use(helmet())
+//cors
+app.use(cors())
 //implementing error handler
 const server=app.listen(PORT,console.log(`server running in Production mode on port ${PORT}`))
 
