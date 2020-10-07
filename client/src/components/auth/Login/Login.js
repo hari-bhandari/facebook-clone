fb68feff44fe955ff53592a4b5c90ba6f77fe84b
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-07 13:18:47 +0100

    added login with google

--- client/src/components/auth/Login/Login.js
+++ client/src/components/auth/Login/Login.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import './Login.css'
+import laptop from './laptop.svg'
 import GoogleLogin from "react-google-login";
 const Login = () => {
     const toggleSignup=()=>{
@@ -37,49 +38,60 @@ const Login = () => {
 
     const getDataFromGoogle=(response)=>{
         console.log(response)
+        // setUser({
+        //     name: '',
+        //     email: '',
+        //     password: '',})
     }
     return (
-        <div className="form-modal">
-
-            <div className="form-toggle">
-                <button id="login-toggle" onClick={toggleLogin}>log in</button>
-                <button id="signup-toggle" onClick={toggleSignup}>sign up</button>
+        <section>
+            <div className="container">
+                <div className="user signinBx">
+                    <div className="imgBx"><img src={'https://raw.githubusercontent.com/WoojinFive/CSS_Playground/master/Responsive%20Login%20and%20Registration%20Form/img1.jpg'} alt="" width={'10px'} height={'20px'}/></div>
+                    <div className="formBx">
+                        <form action="" onSubmit="return false;">
+                            <h2>Sign In</h2>
+                            <input type="text" name="" placeholder="Username"/>
+                            <input type="password" name="" placeholder="Password"/>
+                            <input type="submit" name="" value="Login"/>
+                            <GoogleLogin
+                                clientId="1027946307846-prc53klgk61cijsetp0n0akd5bqj6eqj.apps.googleusercontent.com"
+                                buttonText="Signup with Google"
+                                onSuccess={getDataFromGoogle}
+                                onFailure={()=>{console.log('fail')}}
+                                cookiePolicy={'single_host_origin'}
+                            />
+                            <p className="signup">
+                                Don't have an account ?
+                                <a href="#" onClick="toggleForm();">Sign Up.</a>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+                <div className="user signupBx">
+                    <div className="formBx">
+                        <form action="" onSubmit="return false;">
+                            <h2>Create an account</h2>
+                            <input type="text" name="" placeholder="Username"/>
+                            <input type="email" name="" placeholder="Email Address"/>
+                            <input type="password" name="" placeholder="Create Password"/>
+                            <input type="password" name="" placeholder="Confirm Password"/>
+                            <input type="submit" name="" value="Sign Up"/>
+                            <p className="signup">
+                                Already have an account ?
+                                <a href="#" onClick="toggleForm();">Sign in.</a>
+                            </p>
+                        </form>
+                    </div>
+                    <div className="imgBx"><img src="https://raw.githubusercontent.com/WoojinFive/CSS_Playground/master/Responsive%20Login%20and%20Registration%20Form/img1.jpg" alt=""/></div>
+                </div>
             </div>
+        </section>
+    )
 
-            <div id="login-form">
-                <form>
-                    <input type="text" placeholder="Enter email or username"/>
-                    <input type="password" placeholder="Enter password"/>
-                    <button type="button" className="btn login">login</button>
-                    <p><a href="javascript:void(0)">Forgotten account</a></p>
-                    <hr/>
-                    <button type="button" className="btn -box-sd-effect"> sign in with google</button>
-                    <button type="button" className="btn -box-sd-effect">sign in with Facebook</button>
-                </form>
-            </div>
 
-            <div id="signup-form">
-                <form>
-                    <input type="email" placeholder="Enter your email"/>
-                    <input type="text" placeholder="Choose username"/>
-                    <input type="password" placeholder="Create password"/>
-                    <button type="button" className="btn signup">create account</button>
-                    <p>Clicking <strong>create account</strong> means that you are agree to our <a href="javascript:void(0)">terms of services</a>.</p>
-                    <hr/>
-                    <GoogleLogin
-                        clientId="1027946307846-prc53klgk61cijsetp0n0akd5bqj6eqj.apps.googleusercontent.com"
-                        buttonText="Signup with Google"
-                        onSuccess={getDataFromGoogle}
-                        onFailure={()=>{console.log('fail')}}
-                        cookiePolicy={'single_host_origin'}
-                    />
-                    <button type="button" className="btn -box-sd-effect">sign in with Facebook</button>
-                </form>
-            </div>
 
-        </div>
-
-    );
 };
+
 
 export default Login;
